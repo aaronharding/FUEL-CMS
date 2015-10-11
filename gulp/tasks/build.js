d667a42b7a3913262731001750fcd0a9a4e528e6
Aaron <hiitsaaron@googlemail.com>
2015-10-12 00:05:03 +0200

    gulp file build task created!

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,16 +1,34 @@
 var gulp = require("gulp");
 var minifyCss = require('gulp-minify-css');
-var sourcemaps = require('gulp-sourcemaps');
-// var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+// var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 // var minifyHtml = require('gulp-minify-html');
-// var minifyCss = require('gulp-minify-css');
 // var rev = require('gulp-rev');
 
-var src = "./assets/css/";
-var dest = src + "dist/";
+var src = {
+	css: [ 'assets/css/main.css' ],
+	js: [ '!assets/js/jquery.js', '!assets/js/swfobject.js', '!assets/js/dist/*.js', 'assets/js/**/*.js' ]
+};
+
+var dest = {
+	css: 'assets/css/dist/',
+	js: 'assets/js/dist/'
+};
 
 gulp.task('build', function() {
-	return gulp.src(src + '*.css')
-		.pipe(minifyCss({compatibility: 'ie8'}))
-		.pipe(gulp.dest(dest));
+	gulp.src(src.css)
+		.pipe(minifyCss({compatibility: 'ie8', roundingPrecision: -1, keepSpecialComments: 1}))
+		.pipe(concat('hello.css'))
+		.pipe(gulp.dest(dest.css));
+
+	gulp.src(src.js)
+		.pipe(uglify({
+			output: {
+				max_line_len: 80
+			}
+		}))
+		.pipe(concat('hello.js'))
+		.pipe(gulp.dest(dest.js));
 });
